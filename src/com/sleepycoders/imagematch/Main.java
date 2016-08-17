132d71ea2272c3ddaa0fbbce943c06eeac1ca3aa
Joshua Moody <joshimoo@hotmail.de>
2016-08-18 01:43:07 +0200

    added image output

--- src/com/sleepycoders/imagematch/Main.java
+++ src/com/sleepycoders/imagematch/Main.java
@@ -13,15 +13,14 @@ public class Main {
     public static void main(final String[] args) {
 
         // verify args
-        if(args.length < 2) {
-            System.err.println("example usage: java imagematch src.image sub.image");
+        if(args.length < 3) {
+            System.err.println("example usage: java imagematch src.image sub.image out.image");
             System.exit(-1);
         }
 
         // load images
         Image src = new Image(loadImage(args[0]));
         Image sub = new Image(loadImage(args[1]));
-        BufferedImage out = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
 
         IMatcher matcher = new CorrelationMatcher();
         List<MatchResult> matches = matcher.match(src, sub);
@@ -30,6 +29,25 @@ public class Main {
         matches.sort( (a,b) -> a.likeness > b.likeness ? -1 : a.likeness < b.likeness ? 1 : 0 );
         for (MatchResult r : matches) {
             System.out.println(r.toString());
+        }
+
+        // place sub image on black background at the correct position
+        if(matches.size() > 0) {
+            BufferedImage out = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
+            MatchResult bestMatch = matches.get(0);
+            for (int y = 0; y < sub.getHeight(); y++) {
+                for (int x = 0; x < sub.getWidth(); x++) {
+                    int xOut = x + bestMatch.x;
+                    int yOut = y + bestMatch.y;
+                    int rgb = sub.getRGB(x, y);
+                    out.setRGB(xOut, yOut, rgb);
+                }
+            }
+
+            writeImage(out, args[2]);
+
+        } else {
+            System.out.println("could not find any matches for image \ntry lowering likenessThreshold");
         }
     }
 
@@ -45,7 +63,15 @@ public class Main {
         return img;
     }
 
-    public void writeImage(final BufferedImage img, final String path) {
-
+    public static void writeImage(final BufferedImage img, final String path) {
+        try {
+            // path to file needs to exist already since createNewFile does not create intermediate paths
+            File f = new File(path);
+            f.createNewFile();
+            ImageIO.write(img, "bmp", f);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("image: " + path + " could not be written");
+        }
     }
 }
