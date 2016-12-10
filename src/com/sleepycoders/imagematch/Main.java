1caef2e189491da4ff75fd7efba354b70ffc5aa5
Joshua Moody <joshimoo@hotmail.de>
2016-12-10 18:17:14 +0100

    added commandline parsing for matcher/filter selection

--- src/com/sleepycoders/imagematch/Main.java
+++ src/com/sleepycoders/imagematch/Main.java
@@ -9,22 +9,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
 
 public class Main {
 
-    public static void main(final String[] args) {
-
-        // verify args
-        if(args.length < 3) {
-            System.err.println("example usage: java imagematch src.image sub.image out.image");
-            System.exit(-1);
+    public static Image createMatchImaage(Image src, Image sub, MatchResult match) {
+        Image out = new Image(src.getWidth(), src.getHeight());
+        for (int y = 0; y < sub.getHeight(); y++) {
+            for (int x = 0; x < sub.getWidth(); x++) {
+                int xOut = x + match.x;
+                int yOut = y + match.y;
+                int rgb = sub.getRGB(x, y);
+                out.setRGB(xOut, yOut, rgb);
+            }
         }
 
-        // load images
-        Image src = new Image(loadImage(args[0]));
-        Image sub = new Image(loadImage(args[1]));
+        return out;
+    }
 
-        IMatcher matcher = new CorrelationMatcher();
+    public static List<MatchResult> matchImage(IMatcher matcher, Image src, Image sub) {
         List<MatchResult> matches = matcher.match(src, sub);
 
         // sort the match results based on likeness descendingly
@@ -33,29 +36,74 @@ public class Main {
             System.out.println(r.toString());
         }
 
+        return matches;
+    }
+
+    public static void processMatcherCommand(IMatcher matcher, String srcFile, String outFile, String subFile) {
+
+        // load images
+        Image src = new Image(loadImage(srcFile));
+        Image sub = new Image(loadImage(subFile));
+
+        // process images
+        List<MatchResult> matches = matchImage(matcher, src, sub);
+
         // place sub image on black background at the correct position
         if(matches.size() > 0) {
-            BufferedImage out = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
-            MatchResult bestMatch = matches.get(0);
-            for (int y = 0; y < sub.getHeight(); y++) {
-                for (int x = 0; x < sub.getWidth(); x++) {
-                    int xOut = x + bestMatch.x;
-                    int yOut = y + bestMatch.y;
-                    int rgb = sub.getRGB(x, y);
-                    out.setRGB(xOut, yOut, rgb);
-                }
-            }
-
-            writeImage(out, args[2]);
-
+            Image out = createMatchImaage(src, sub, matches.get(0));
+            writeImage(out, outFile);
         } else {
             System.out.println("could not find any matches for image \ntry lowering likenessThreshold");
         }
+    }
 
-        // TODO: let's try some filters & effects
-        if(args.length > 3) {
-            Effect.apply(src, Effect.sepiaEffect);
-            writeImage(src, args[3]);
+    public static void processFilterCommand(IntUnaryOperator filter, String srcFile, String outFile) {
+        Image src = new Image(loadImage(srcFile));
+        Effect.apply(src, filter);
+        writeImage(src, outFile);
+    }
+
+    public static void main(final String[] args) {
+
+        // verify args
+        if(args.length < 4) {
+            System.err.println("example usage: java imagematch command option src.image out.image [sub.image]");
+            System.exit(-1);
+        }
+
+        // TODO: switch to commons cli or args4j
+        // this is just quick and dirty command line parsing
+        switch(args[0]) {
+            case "match":
+                if(args.length < 5) {
+                    System.err.println("missing required Command Line Parameters");
+                    System.err.println("example usage: java imagematch matcher naiv src.image out.image sub.image");
+                    System.exit(-1);
+                }
+
+                IMatcher matcher;
+                switch (args[1]) {
+                    case "exact": matcher = new ExactMatcher(); break;
+                    case "naiv": matcher = new NaivMatcher(); break;
+                    case "corr": matcher = new CorrelationMatcher(); break;
+                    default: throw new IllegalArgumentException("unknown matcher specified");
+                }
+                System.out.println("using matcher: " + args[1] + "\nsrc: " + args[2] + "\nsub: " + args[4] + "\nout: " + args[3]);
+                processMatcherCommand(matcher, args[2], args[3], args[4]);
+            break;
+
+            case "filter":
+                IntUnaryOperator filter;
+                switch (args[1]) {
+                    case "sepia": filter = Effect.sepiaEffect; break;
+                    case "red": filter = Effect.redEffect; break;
+                    case "green": filter = Effect.greenEffect; break;
+                    case "blue": filter = Effect.blueEffect; break;
+                    default: throw new IllegalArgumentException("unknown filter specified");
+                }
+                System.out.println("applying filter: " + args[1] + "\nsrc: " + args[2] + "\nout: " + args[3]);
+                processFilterCommand(filter, args[2], args[3]);
+            break;
         }
     }
 
@@ -76,7 +124,7 @@ public class Main {
             // path to file needs to exist already since createNewFile does not create intermediate paths
             File f = new File(path);
             f.createNewFile();
-            ImageIO.write(img, "bmp", f);
+            ImageIO.write(img, "png", f);
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("image: " + path + " could not be written");
